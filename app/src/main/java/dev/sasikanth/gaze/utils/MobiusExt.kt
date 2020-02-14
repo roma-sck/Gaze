88690e1dfc7a8286a2736910f2a34c5abdb52f99
Sasikanth Miriyampalli <sasikanthmiriyampalli@gmail.com>
2020-02-14 06:57:38 +0530

    Add mobius extensions

---
+++ app/src/main/java/dev/sasikanth/gaze/utils/MobiusExt.kt
@@ -0,0 +1,19 @@
+package dev.sasikanth.gaze.utils
+
+import com.spotify.mobius.First
+import com.spotify.mobius.Next
+
+fun <M, F> first(model: M, vararg effects: F): First<M, F> = if (effects.isEmpty()) {
+  First.first(model)
+} else {
+  First.first(model, setOf(*effects))
+}
+
+fun <M, F> next(model: M, vararg effects: F): Next<M, F> = if (effects.isEmpty()) {
+  Next.next(model)
+} else {
+  Next.next(model, setOf(*effects))
+}
+
+fun <M, F> dispatch(effect: F, vararg effects: F): Next<M, F> =
+  Next.dispatch<M, F>(setOf(effect, *effects))
