9e8119b93cb2481f3d125478b7ddb8751caa570e
Sasikanth Miriyampalli <sasikanthmiriyampalli@gmail.com>
2020-02-15 18:51:31 +0530

    Enable moshi to convert `LocalDate` class using `DateConverters`

--- app/src/main/java/dev/sasikanth/gaze/di/AppModule.kt
+++ app/src/main/java/dev/sasikanth/gaze/di/AppModule.kt
@@ -2,17 +2,22 @@ package dev.sasikanth.gaze.di
 
 import android.content.Context
 import androidx.room.Room
+import com.squareup.moshi.Moshi
 import dagger.Module
 import dagger.Provides
 import dev.sasikanth.gaze.AppDatabase
 import dev.sasikanth.gaze.image.NasaPictureApi
 import dev.sasikanth.gaze.utils.AppDispatcherProvider
+import dev.sasikanth.gaze.utils.DateConverters
 import dev.sasikanth.gaze.utils.DispatcherProvider
 import dev.sasikanth.gaze.utils.PacificClock
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.threeten.bp.Clock
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.create
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
@@ -29,9 +34,26 @@ object AppModule {
   @Provides
   @Singleton
   fun providesNasaPicturesApi(): NasaPictureApi {
+    // Creating log interceptors with basic level
+    val interceptor = HttpLoggingInterceptor()
+    interceptor.level = HttpLoggingInterceptor.Level.BASIC
+
+    // OkHttp Client
+    // Adding log interceptors and setting timeouts
+    val client = OkHttpClient.Builder()
+      .addInterceptor(interceptor)
+      .readTimeout(2, TimeUnit.MINUTES)
+      .connectTimeout(2, TimeUnit.MINUTES)
+      .build()
+
+    val moshi = Moshi.Builder()
+      .add(DateConverters)
+      .build()
+
     val retrofit = Retrofit.Builder()
       .baseUrl("https://api.nasa.gov/")
-      .addConverterFactory(MoshiConverterFactory.create())
+      .client(client)
+      .addConverterFactory(MoshiConverterFactory.create(moshi))
       .build()
 
     return retrofit.create()
--- app/src/main/java/dev/sasikanth/gaze/utils/DateConverters.kt
+++ app/src/main/java/dev/sasikanth/gaze/utils/DateConverters.kt
@@ -1,18 +1,22 @@
 package dev.sasikanth.gaze.utils
 
 import androidx.room.TypeConverter
+import com.squareup.moshi.FromJson
+import com.squareup.moshi.ToJson
 import org.threeten.bp.LocalDate
 
 object DateConverters {
 
   @TypeConverter
   @JvmStatic
+  @FromJson
   fun toDate(value: String): LocalDate {
     return LocalDate.parse(value)
   }
 
   @TypeConverter
   @JvmStatic
+  @ToJson
   fun fromDate(date: LocalDate): String {
     return date.toString()
   }
