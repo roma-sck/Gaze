2ce87ef665bbbc2d546221d6bc7d824159d0ec2d
Sasikanth Miriyampalli <sasikanthmiriyampalli@gmail.com>
2020-02-15 13:39:38 +0530

    Add database queries for `GazeImage`

--- app/src/main/java/dev/sasikanth/gaze/image/GazeImage.kt
+++ app/src/main/java/dev/sasikanth/gaze/image/GazeImage.kt
@@ -1,5 +1,31 @@
 package dev.sasikanth.gaze.image
 
+import androidx.room.Dao
+import androidx.room.Entity
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.PrimaryKey
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 import org.threeten.bp.LocalDate
 
-data class GazeImage(val date: LocalDate)
+@Entity(tableName = "images")
+data class GazeImage(
+  @PrimaryKey
+  val date: LocalDate
+) {
+
+  @Dao
+  interface RoomDao {
+
+    @Query("""
+      SELECT * FROM images
+      WHERE media_type = 'image'
+      ORDER BY date(date) DESC
+    """)
+    fun images(): Flow<List<GazeImage>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(images: List<GazeImage>)
+  }
+}
