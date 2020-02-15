94d2e7489969c9366bba078d3138ac0439f7b039
Sasikanth Miriyampalli <sasikanthmiriyampalli@gmail.com>
2020-02-15 18:07:28 +0530

    Add NavHostFragment to activity_main
    - Add `ImagesGridFragment` to nav_graph

---
+++ app/src/main/java/dev/sasikanth/gaze/imagesgrid/ImagesGridFragment.kt
@@ -0,0 +1,7 @@
+package dev.sasikanth.gaze.imagesgrid
+
+import androidx.fragment.app.Fragment
+import dev.sasikanth.gaze.R
+
+class ImagesGridFragment : Fragment(R.layout.fragment_images_grid) {
+}
