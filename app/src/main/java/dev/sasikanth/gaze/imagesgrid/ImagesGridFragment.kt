f13c868e4b79748bec3683174d35e2d7d73dd0e0
Sasikanth Miriyampalli <sasikanthmiriyampalli@gmail.com>
2020-02-16 09:49:57 +0530

    Implement `ImagesGridUi` in `ImagesGridFragment`

--- app/src/main/java/dev/sasikanth/gaze/imagesgrid/ImagesGridFragment.kt
+++ app/src/main/java/dev/sasikanth/gaze/imagesgrid/ImagesGridFragment.kt
@@ -1,7 +1,83 @@
 package dev.sasikanth.gaze.imagesgrid
 
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.spotify.mobius.android.MobiusLoopViewModel
 import dev.sasikanth.gaze.R
+import dev.sasikanth.gaze.image.GazeImage
+import dev.sasikanth.gaze.imagesgrid.ImagesGridAdapter.Companion.GRID_ITEM_TYPE_IMAGE
+import dev.sasikanth.gaze.imagesgrid.ImagesGridAdapter.Companion.GRID_ITEM_TYPE_PROGRESS
+import kotlinx.android.synthetic.main.fragment_images_grid.*
 
-class ImagesGridFragment : Fragment(R.layout.fragment_images_grid) {
+class ImagesGridFragment : Fragment(R.layout.fragment_images_grid), ImagesGridUi {
+
+  private val viewModel: MobiusLoopViewModel<ImagesGridModel, ImagesGridEvent, ImagesGridEffect, ImagesGridViewEffect> by activityViewModels()
+  private val gridAdapter = ImagesGridAdapter()
+
+  private val uiRenderer by lazy {
+    ImagesGridUiRenderer(this)
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+    viewModel.models.observe(viewLifecycleOwner, Observer { model ->
+      uiRenderer.render(model)
+    })
+
+    imagesGrid.apply {
+
+      (layoutManager as GridLayoutManager).apply {
+        spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+          override fun getSpanSize(position: Int): Int {
+            return when (val itemViewType = gridAdapter.getItemViewType(position)) {
+              GRID_ITEM_TYPE_IMAGE -> 1
+              GRID_ITEM_TYPE_PROGRESS -> 2
+              else -> throw IllegalArgumentException("Unknown item view type: $itemViewType")
+            }
+          }
+        }
+      }
+
+      adapter = gridAdapter
+
+      addOnScrollListener(object : RecyclerView.OnScrollListener() {
+        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+          super.onScrollStateChanged(recyclerView, newState)
+          val lastItemPosition = gridAdapter.itemCount - 1
+          val itemViewType = gridAdapter.getItemViewType(lastItemPosition)
+
+          if (itemViewType == GRID_ITEM_TYPE_IMAGE) {
+            if (!recyclerView.canScrollVertically(1)) {
+              viewModel.dispatchEvent(ImagesListReachedEnd)
+            }
+          }
+        }
+      })
+    }
+  }
+
+  override fun showProgress() {
+    imagesProgress.isVisible = true
+    imagesGrid.isVisible = false
+  }
+
+  override fun hideProgress() {
+    imagesProgress.isVisible = false
+    imagesGrid.isVisible = true
+  }
+
+  override fun showImages(images: List<GazeImage>) {
+    val gridImages = images
+      .map { ImageGridItem(it) }
+    val gridItems = gridImages + listOf(ProgressGridItem)
+
+    gridAdapter.submitList(gridItems)
+  }
 }
